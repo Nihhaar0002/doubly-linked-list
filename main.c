5296c5ceadcf39ae4c92c2d14bdace7984ac3eff
Nihhaar Saini <msnrk2006@hmail.com>
2025-11-03 08:22:03 +0530

    Added insert-at-position feature to linked list

--- main.c
+++ main.c
@@ -85,6 +85,37 @@ void display() {
     }
     printf("\n");
 }
+void ins_pos() {
+    int pos;
+    printf("\nEnter the position where the node should be inserted: ");
+    scanf("%d", &pos);
+
+    if (pos < 1 || pos > count() + 1) {
+        printf("\nInvalid position!\n");
+        return;
+    }
+
+    if (pos == 1) {
+        ins_beg();
+        return;
+    }
+
+    struct node *newnode = create_node();
+    temp = head;
+    for (int i = 1; i < pos - 1; i++) {
+        temp = temp->next;
+    }
+
+    newnode->next = temp->next;
+    newnode->prev = temp;
+
+    if (temp->next != NULL) {
+        temp->next->prev = newnode;
+    }
+
+    temp->next = newnode;
+    printf("\n%d is inserted at position %d\n", newnode->data, pos);
+}
 
 // Main menu
 int main() {
